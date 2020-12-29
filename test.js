1ba782eb82a697c3fda88805f06c8bbc215e3239
Eisuke (Tomato) <ekke.ettomato@outlook.jp>
2020-12-29 17:40:48 +0900

    スプシ結果を返答できるように下

--- test.js
+++ test.js
@@ -6,16 +6,17 @@ const fetch = require('node-fetch');
 const api_url = 'https://script.google.com/macros/s/AKfycbz2XxvmlqZC2gsYjPn1BAAJxwdcR5Cth_5_ef-ef-7yZ8zSQAo/exec'
 
 fetch(api_url, {method: 'GET'}).then((res) => {
-console.log( res );
-return res.json(); 
+    return res.json(); 
 })
 .then((data) => {
-log(data);
+    const totalResult = data[0].Total_points
+    console.log(totalResult);
+    console.log(returnText(totalResult));
 })
 .catch((err) => {
 console.error( err );
 });
 
-const log = jsonObj => {
-    return console.log(jsonObj);
-}
+const returnText = jsonObj => {
+    return {type: 'text', text: '成績は' + jsonObj + 'です。'};
+  }
